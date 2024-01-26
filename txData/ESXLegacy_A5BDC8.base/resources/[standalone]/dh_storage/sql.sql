2189d25d2ca9efede519cfeccebfdbc526d692ea
Unknown Development <130213414+Flyboyztruly@users.noreply.github.com>
2024-01-26 13:17:56 -0800

    first upload

--- txData/ESXLegacy_A5BDC8.base/resources/[standalone]/dh_storage/sql.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `dh_storage` (
-  `identifier` varchar(50) DEFAULT NULL,
-  `tables` longtext DEFAULT NULL,
-  UNIQUE KEY `identifier` (`identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
