086cb9830beed8205b9a53e7642f923c015e4a11
Unknown Development <130213414+Flyboyztruly@users.noreply.github.com>
2024-01-25 19:02:42 -0800

    first upload

---
+++ txData/ESXLegacy_A5BDC8.base/resources/[props]/collection_props/fxmanifest.lua
@@ -0,0 +1,44 @@
+-- FX Information --
+fx_version 'cerulean'
+game 'gta5'
+author 'DJ'
+description 'Collection Lifetime Bundle Pack'
+version '1.0.3'
+lua54 'yes'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/cocktailv1/cocktails.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/cocktailv2/alcholicdrinksvtwo.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/cookiesandbrownies/browniesandcookies.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/donuts/donuts.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/frappuccino/frappuccino.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/happydads/happydads.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/icecream/icecream.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/italianfood/italianfood.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/lush/lush.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/mexicanfood/mexicanfood.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mcdonalds/mcdonalds.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/pizza/pizzas.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/kbbq/kbbq.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/flask/flask.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/pringles/pringles.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/sandwiches/sandwichs.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/seafood/seafood.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/spoon/spoon.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/sushi/sushi.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/talentiicecream/talentiicecream.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/togochinesefood/togochinesefood.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/togocups/togocups.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/whiteclaws/whiteclaws.ytyp' --done
+data_file 'DLC_ITYP_REQUEST' 'stream/weedessentials/weedessentials.ytyp' --done
+data_file 'DLC_ITYP_REQUEST' 'stream/wingsandfries/wings.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/cerealbowls/cerealbowls.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/breakfast/breakfast.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sushiv2/sushivtwo.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/jamaican/jamaican.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/steakhouse/steakhouse.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/beanmachine/cafe.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/uwucafe/uwuprops.ytyp'
+
+dependency '/assetpacks'
+dependency '/assetpacks'
---
+++ txData/ESXLegacy_A5BDC8.base/resources/[props]/collection_props/readme.txt
@@ -0,0 +1,6 @@
+----[INFORMATION]-----
+Having Problems with any props please make a ticket in the discord server
+https://discord.gg/pGqGXajFc2
+
+For any pop placements and picture you can check out #documents inside the discord server or
+https://djs-collection.gitbook.io/product-docs/overview/about-us
