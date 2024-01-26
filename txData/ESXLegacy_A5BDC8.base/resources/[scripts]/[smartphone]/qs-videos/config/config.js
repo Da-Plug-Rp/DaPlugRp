c1b3507f2f564fa6b14ef5338bade248029a969e
Unknown Development <130213414+Flyboyztruly@users.noreply.github.com>
2024-01-25 19:19:04 -0800

    first upload

---
+++ txData/ESXLegacy_A5BDC8.base/resources/[scripts]/[smartphone]/qs-videos/config/config.js
@@ -0,0 +1,3 @@
+Config = []
+
+Config.Webhook = 'https://discord.com/api/webhooks/1192698586538983514/g5TfKe2ooYNmN3DY4XS5jIJqoXR_j8EI3AsJF74CI_3QWyPpbIYTNA8fnG3JEW54DIke' // Set your own discord Webhook here.
