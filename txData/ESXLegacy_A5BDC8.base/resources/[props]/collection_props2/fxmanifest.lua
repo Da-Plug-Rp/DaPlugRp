35a18b84a109900d5f930acd739efd7a3162c815
Unknown Development <130213414+Flyboyztruly@users.noreply.github.com>
2024-01-25 19:06:06 -0800

    first upload

---
+++ txData/ESXLegacy_A5BDC8.base/resources/[props]/collection_props2/fxmanifest.lua
@@ -0,0 +1,27 @@
+-- FX Information --
+fx_version 'cerulean'
+game 'gta5'
+author 'DJ'
+description 'Collection Lifetime Bundle Pack'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/babycribs/babycribs.ytyp' --done
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/beanbags/beanbags.ytyp' --done
+data_file 'DLC_ITYP_REQUEST' 'stream/bedsv1/beds.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/beebabyshower/beebabyshower.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/brickbears/brickbears.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/childhoodrugs/childhoodrugs.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/consoles/consoles.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/gamingchairs/gamingchairs.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/jblspeaker/jblspeaker.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/lush/lush.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/petessentials/petessentials.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/takashirugs/takashirugs.ytyp' -- done
+data_file 'DLC_ITYP_REQUEST' 'stream/tattooprops/tattooprops.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/cleaning/cleanprops.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mechanic/mechanictools.ytyp'
+
+dependency '/assetpacks'
+dependency '/assetpacks'
