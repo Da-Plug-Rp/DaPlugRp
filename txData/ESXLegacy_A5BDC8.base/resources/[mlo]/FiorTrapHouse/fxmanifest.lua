96a7768e8866de63642cbc91de846f90de7d5235
Unknown Development <130213414+Flyboyztruly@users.noreply.github.com>
2024-01-25 19:00:01 -0800

    first upload

---
+++ txData/ESXLegacy_A5BDC8.base/resources/[mlo]/FiorTrapHouse/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DMO'
+description 'FiorTrapHouse'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+-- (for Linked ytd if decide to use)
+--file 'meta/gtxd.meta'
+
+files {
+    
+
+}
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/hoodframe.ytyp.ityp'
+data_file 'DLC_ITYP_REQUEST' 'stream/Pillars.ytyp.ityp'
+data_file 'DLC_ITYP_REQUEST' 'stream/house1test.ytyp.ityp'
+
+-- (for Linked ytd if decide to use)
+--data_file 'GTXD_PARENTING_DATA' 'meta/gtxd.meta' 
