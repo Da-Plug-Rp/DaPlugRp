d3727cd15f53a51d8a208e69744829d622e7e8cf
Unknown Development <130213414+Flyboyztruly@users.noreply.github.com>
2024-01-25 19:22:38 -0800

    first upload

---
+++ txData/ESXLegacy_A5BDC8.base/resources/[standalone]/pickle_payment/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.Debug = true
+
+Config.Language = "en"
+
+Config.Registers = {
+    {
+        Title = "Ammunation",
+        AllowedGroups = {["ammunation"] = 0},
+        Payment = {
+            Type = "player",
+            Society = "ammunation"
+        },
+        Coords = vector3(23.8267, -1105.9731, 29.7970),
+    }
+}
