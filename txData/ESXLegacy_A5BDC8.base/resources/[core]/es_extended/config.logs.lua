9c91a2fa6fe00ede30f71164c0f61edca697e567
Unknown Development <130213414+Flyboyztruly@users.noreply.github.com>
2024-01-25 16:22:01 -0800

    first upload

---
+++ txData/ESXLegacy_A5BDC8.base/resources/[core]/es_extended/config.logs.lua
@@ -0,0 +1,23 @@
+Config.DiscordLogs = {
+    Webhooks = {
+        default = 'https://discord.com/api/webhooks/1192648476052426902/N78f2ClihusEOwJSXW9GwWHcUutPNNFxPfWLsuHjz5NMpp8ysjcbv3omaLLng6NOsXNC',
+        test = 'https://discord.com/api/webhooks/1192648476052426902/N78f2ClihusEOwJSXW9GwWHcUutPNNFxPfWLsuHjz5NMpp8ysjcbv3omaLLng6NOsXNC',
+        Chat = 'https://discord.com/api/webhooks/1192648476052426902/N78f2ClihusEOwJSXW9GwWHcUutPNNFxPfWLsuHjz5NMpp8ysjcbv3omaLLng6NOsXNC',
+        UserActions = 'https://discord.com/api/webhooks/1192648476052426902/N78f2ClihusEOwJSXW9GwWHcUutPNNFxPfWLsuHjz5NMpp8ysjcbv3omaLLng6NOsXNC',
+        Resources = 'https://discord.com/api/webhooks/1192648476052426902/N78f2ClihusEOwJSXW9GwWHcUutPNNFxPfWLsuHjz5NMpp8ysjcbv3omaLLng6NOsXNC',
+        Paycheck = 'https://discord.com/api/webhooks/1192648476052426902/N78f2ClihusEOwJSXW9GwWHcUutPNNFxPfWLsuHjz5NMpp8ysjcbv3omaLLng6NOsXNC'
+    },
+
+    Colors = { -- https://www.spycolor.com/
+        default = 14423100,
+        blue = 255,
+        red = 16711680,
+        green = 65280,
+        white = 16777215,
+        black = 0,
+        orange = 16744192,
+        yellow = 16776960,
+        pink = 16761035,
+        lightgreen = 65309
+    }
+}
