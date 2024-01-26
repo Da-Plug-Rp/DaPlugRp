2f2d5cadadd94babc828f6f1c6ca6e7b821f55b2
Unknown Development <130213414+Flyboyztruly@users.noreply.github.com>
2024-01-25 19:01:16 -0800

    first upload

---
+++ txData/ESXLegacy_A5BDC8.base/resources/[mlo]/hunter_bank_wf/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game { 'gta5' }
+this_is_a_map 'yes'
+
+author 'HUNTER#5979'
+description 'Walls Fargo Bank By MrHunter'
+version '1.0.2'
+
+
+escrow_ignore {
+	'stream/*.ymf',
+	'stream/*.ymap'
+}
+
+files {
+    "interiorproxies.meta"
+}
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+dependency '/assetpacks'
+dependency '/assetpacks'
